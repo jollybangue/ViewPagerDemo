2beafbc96e1ba3d099a1e8d43979aabdaabbfed3
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-19 21:15:19 -0600

    Initial commit of the Android app ViewPagerDemo
    A small Android app to experiment view pager features.

---
+++ app/src/main/java/com/example/viewpagerdemo/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.viewpagerdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+
+        val images = listOf(
+            R.drawable.animal_1,
+            R.drawable.animal_2,
+            R.drawable.animal_3,
+            R.drawable.animal_4,
+            R.drawable.animal_5,
+            R.drawable.animal_6,
+            R.drawable.animal_7,
+            R.drawable.animal_8,
+            //R.drawable.animal_9,
+            R.drawable.animal_10,
+            )
+
+        val adapter = ViewPagerAdapter(images)
+        viewPager.adapter = adapter
+    }
+}
